a1c05640c106cf55b34c2122b53ff4c53b6cbc6f
Abdullah <Abdullah@gg.com>
2025-12-01 15:46:33 +0300

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   hello.cs

---
+++ hello.cs
@@ -0,0 +1,13 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        HelloWorld h = new HelloWorld();
+        Console.WriteLine(x);
+    }
+}
+
+class HelloWorld
+{
+    var x = "HelloWorld";
+}
